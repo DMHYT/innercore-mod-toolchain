a58e047fe5b002cb1d93e3521eb5a7904517b8b4
vsdum <danmarkhunter13072006@gmail.com>
2021-11-12 19:57:59 +0200

    FIxes

--- toolchain-mod/initialize_environment.py
+++ toolchain-mod/initialize_environment.py
@@ -2,14 +2,31 @@
 YOU HAVE TO RUN THIS SCRIPT WHEN YOU HAVE JUST
 INITIALIZED THE PROJECT OR CLONED YOUR PROJECT'S
 GITHUB REPOSITORY. WITHOUT RUNNING IT, NONE OF
-THE BUILD TASKS WILL WORK
+THE BUILD TASKS WILL WORK.
+AFTER RUNNING THIS SCRIPT, PRESS CTRL+SHIFT+B AND
+RUN `Initialize Environment` TASK,
+FOR ALL OTHER NEEDED FILES TO DOWNLOAD
 '''
 
 from urllib.request import urlretrieve
 from zipfile import ZipFile
-from os import remove, getcwd
-from os.path import join
+from os import remove, getcwd, listdir, mkdir
+from os.path import join, exists, isfile, isdir
+from shutil import copy2, rmtree
 from subprocess import call
+
+def copytree(src, dst, symlinks=False, ignore=None):
+    if not exists(src) or isfile(src):
+        raise Exception()
+    for item in listdir(src):
+        s = join(src, item)
+        d = join(dst, item)
+        if isdir(s):
+            if not exists(d):
+                mkdir(d)
+            copytree(s, d, symlinks, ignore)
+        else:
+            copy2(s, d)
 
 print("Initializing development environment...")
 url = "https://codeload.github.com/DMHYT/innercore-mod-toolchain/zip/gitignored-toolchain"
@@ -18,5 +35,8 @@ urlretrieve(url=url, filename=archive_path)
 with ZipFile(archive_path, 'r') as archive:
     archive.extractall(join(getcwd(), "toolchain"))
 remove(archive_path)
-call(["python", join(getcwd(), "toolchain", "python", "task.py"), "loadDocs loadJavaDependencies loadAdbAndBin downloadICHeaders downloadGnustlHeaders downloadNdkIfNeeded"])
+toolchain_path = join(getcwd(), "toolchain")
+shit = join(toolchain_path, "innercore-mod-toolchain-gitignored-toolchain")
+copytree(shit, toolchain_path)
+rmtree(shit)
 print("complete!")
